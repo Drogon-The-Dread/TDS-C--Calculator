b3254e6b12bab4eb8e70088cbea5cb9dfa711843
Drogon-The-Dread <lowneyconor@gmail.com>
2024-02-01 15:13:51 +0000

    RED: Create failing test for Calculator Class

---
+++ CalculatorXUnitTests/Classes/CalculatorClassUnitTests.cs
@@ -0,0 +1,17 @@
+namespace CalculatorXUnitTests;
+
+public class CalculatorClassTests
+{
+    [Theory]
+    [InlineData("1 + 1", 2)]
+    [InlineData("2 + 3", 5)]
+    [InlineData("10 + 20", 30)]
+    public void Evaluate_Addition_ReturnsCorrectResult(string expression, double expected)
+    {
+        var calculator = new Calculator();
+
+        double result = calculator.Evaluate(expression);
+
+        Assert.Equal(expected, result);
+    }
+}
